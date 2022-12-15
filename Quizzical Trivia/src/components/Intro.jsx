8d8d43c49d2724273892543e1eb070fc6d8de1d5
Salman Khan <salmankhan231999@gmail.com>
2022-12-16 04:40:16 +0530

    quizzical trivia app setup

---
+++ Quizzical Trivia/src/components/Intro.jsx
@@ -0,0 +1,13 @@
+import "../styles/intro.css";
+
+const Intro = () => {
+  return (
+    <header className="intro--header">
+      <h1>Quizzical</h1>
+      <p>Some Description if needed</p>
+      <button>Start Quiz</button>
+    </header>
+  );
+};
+
+export default Intro;

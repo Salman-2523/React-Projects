1d71a5a54dcc4d3abb76abc73108532238ef073d
Salman Khan <salmankhan231999@gmail.com>
2022-12-17 15:59:18 +0530

    play again - feature

--- Quizzical Trivia/src/components/Quiz.jsx
+++ Quizzical Trivia/src/components/Quiz.jsx
@@ -4,6 +4,28 @@ import "../styles/quiz.css";
 import SingleQuestion from "./SingleQuestion";
 const Quiz = () => {
   const [questions, setQuestions] = useState([]);
+  const [submit, setSubmit] = useState(false);
+  const [count, setCount] = useState(0);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [playAgain, setPlayAgain] = useState(false);
+
+  const incrementCounter = () => setCount((prevCount) => prevCount + 1);
+  const handleSubmit = () => {
+    console.log("submit", count);
+    setSubmit(true);
+  };
+
+  const handlePlayAgain = () => {
+    setPlayAgain(true);
+    setSubmit((prevState) => !prevState);
+    setCount(0);
+    setCorrectAnswers([]);
+  };
+
+  //   console.log(count);
+  // we have questions array through which we can check if the user had submitted wrong answer
+  // if yes don't let user submit another answer and keep the count of correct answers
+  // check with another if condition and then keep the count
 
   useEffect(() => {
     fetch(
@@ -11,18 +33,52 @@ const Quiz = () => {
     )
       .then((res) => res.json())
       .then(({ results }) => setQuestions(results));
-  }, []);
-//   console.log(questions);
+  }, [playAgain]);
+  console.log(correctAnswers);
   return (
-    <main className="quiz--container">
-      {questions.length > 0 &&
-        questions.map((question) => {
-          return <SingleQuestion question={question} key = {nanoid()}/>;
-        })}
-      <div className="quiz--button--container">
-        <button className="quiz--button">Check answers</button>
-      </div>
-    </main>
+    <>
+      {questions.length === 0 ? (
+        <h1 style={{ textAlign: "center" }}>App Loading</h1>
+      ) : (
+        <>
+          <main className="quiz--container">
+            {questions.length > 0 &&
+              questions.map((question) => {
+                return (
+                  <SingleQuestion
+                    question={question}
+                    key={nanoid()}
+                    incrementCounter={incrementCounter}
+                    correctAnswers={correctAnswers}
+                    setCorrectAnswers={setCorrectAnswers}
+                  />
+                );
+              })}
+
+            <div className="quiz--button--container">
+              {submit ? (
+                <>
+                  <span>
+                    You have Scored {count} out of {questions.length}
+                  </span>
+                  <button className="quiz--button" onClick={handlePlayAgain}>
+                    Play again
+                  </button>
+                </>
+              ) : (
+                <button
+                  className="quiz--button"
+                  onClick={handleSubmit}
+                  disabled={count < questions.length}
+                >
+                  Check answers
+                </button>
+              )}
+            </div>
+          </main>
+        </>
+      )}
+    </>
   );
 };
 
--- Quizzical Trivia/src/components/SingleQuestion.jsx
+++ Quizzical Trivia/src/components/SingleQuestion.jsx
@@ -1,25 +1,49 @@
 import { nanoid } from "nanoid";
+import { useState } from "react";
 import "../styles/singlequestion.css";
 
-const SingleQuestion = ({question}) => {
-    //store the correct answer at the random index else it will be obvious to guess the answer
-    const correctAnswer = Math.ceil(Math.random() * 3)
-    const options = [...question.incorrect_answers]
-    options.splice(correctAnswer,0,question.correct_answer)
-//    console.log(options); 
-//    console.log(question.correct_answer);
-   
-  return (
-      <div className = "singlequestion">
-        <h2>{question.question}</h2>
-        <div className="options--container">
-           {options.length && options.map(option=>{
-            return <div key = {nanoid()}>{option}</div>
-           })}
-        </div>
-        <hr />
-      </div>
-  )
-}
+const SingleQuestion = ({
+  question,
+  incrementCounter,
+  correctAnswers,
+  setCorrectAnswers,
+}) => {
+  //store the correct answer at the random index else it will be obvious to guess the answer
+  const correctAnswer = Math.ceil(Math.random() * 3);
+  const options = [...question.incorrect_answers];
+  //   const [selected, setSelected] = useState(false);
+  options.splice(correctAnswer, 0, question.correct_answer);
 
-export default SingleQuestion
+  const handleChooseOption = (option) => {
+    let select = correctAnswers.includes(question.correct_answer);
+    if (select) {
+      return;
+    }
+
+    if (option === question.correct_answer) {
+      incrementCounter();
+      setCorrectAnswers((prevState) => {
+        return [...prevState, option];
+      });
+    }
+  };
+
+  return (
+    <div className="singlequestion">
+      <h2>{question.question}</h2>
+      <div className="options--container">
+        {options.length &&
+          options.map((option) => {
+            return (
+              <div key={nanoid()} onClick={() => handleChooseOption(option)}>
+                {option}
+              </div>
+            );
+          })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default SingleQuestion;
